e373a7376968da9c95cb973e974b09cd07a34418
Oksana Kormilina <keysb@mail.ru>
2024-02-21 19:28:33 +0400

    Add deleting event

--- src/components/Events/EventDetails.jsx
+++ src/components/Events/EventDetails.jsx
@@ -1,17 +1,34 @@
-import { Link, Outlet, useParams } from "react-router-dom";
-import { useQuery } from "@tanstack/react-query";
+import { Link, Outlet, useParams, useNavigate } from "react-router-dom";
+import { useQuery, useMutation } from "@tanstack/react-query";
 
 import Header from "../Header.jsx";
-import { fetchEvent } from "../../util/http.js";
+import { fetchEvent, deleteEvent } from "../../util/http.js";
 import ErrorBlock from "../UI/ErrorBlock.jsx";
 
 export default function EventDetails() {
   const { id } = useParams();
+  const navigate = useNavigate();
 
   const { data, isPending, isError, error } = useQuery({
     queryKey: ["events", { search: id }],
     queryFn: ({ signal }) => fetchEvent({ id, signal }),
   });
+
+  const {
+    mutate,
+    isPending: isMutationPending,
+    isError: isMutationError,
+    error: mutationError,
+  } = useMutation({
+    mutationFn: deleteEvent,
+    onSuccess: () => {
+      navigate("/events");
+    },
+  });
+
+  function handleDelete() {
+    mutate({ id });
+  }
   return (
     <>
       <Outlet />
@@ -27,13 +44,19 @@ export default function EventDetails() {
           message={error.info?.message || "Could not fetch event details"}
         />
       )}
+      {isMutationPending && <p>Deleting event...</p>}
+      {isMutationError && (
+        <ErrorBlock
+          title="Deleting error"
+          message={mutationError.info?.message || "Could not delete event"}
+        />
+      )}
       {data && (
-        
         <article id="event-details">
           <header>
             <h1>{data.title}</h1>
             <nav>
-              <button>Delete</button>
+              <button onClick={handleDelete}>Delete</button>
               <Link to="edit">Edit</Link>
             </nav>
           </header>
@@ -42,7 +65,9 @@ export default function EventDetails() {
             <div id="event-details-info">
               <div>
                 <p id="event-details-location">{data.location}</p>
-                <time dateTime={`Todo-DateT$Todo-Time`}>{data.date} @ {data.time}</time>
+                <time dateTime={`Todo-DateT$Todo-Time`}>
+                  {data.date} @ {data.time}
+                </time>
               </div>
               <p id="event-details-description">{data.description}</p>
             </div>
