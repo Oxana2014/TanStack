4eabebf4ecdbf08f4802d28c5619437078f282ba
Oksana Kormilina <keysb@mail.ru>
2024-02-22 15:28:49 +0400

    use Tanstack with React Router, add global progress bar

--- src/components/Events/EditEvent.jsx
+++ src/components/Events/EditEvent.jsx
@@ -1,4 +1,11 @@
-import { Link, useNavigate, useParams } from "react-router-dom";
+import {
+  Link,
+  redirect,
+  useNavigate,
+  useParams,
+  useSubmit,
+  useNavigation,
+} from "react-router-dom";
 import { useQuery, useMutation } from "@tanstack/react-query";
 
 import Modal from "../UI/Modal.jsx";
@@ -10,32 +17,38 @@ import ErrorBlock from "../UI/ErrorBlock.jsx";
 export default function EditEvent() {
   const navigate = useNavigate();
   const params = useParams();
+  const submit = useSubmit();
+  const { state } = useNavigation();
+
+  //continue using to have cashed queries
   const { data, isPending, isError, error } = useQuery({
     queryKey: ["events", params.id],
     queryFn: ({ signal }) => fetchEvent({ signal, id: params.id }),
+    staleTime: 10000
   });
 
-  const { mutate } = useMutation({
-    mutationFn: updateEvent,
-    onMutate: async (data) => {
-      //this data is arguments for mutate()
-      await queryClient.cancelQueries({ queryKey: ["events", params.id] });
-      const previousEvent = queryClient.getQueryData(["events", params.id]);
-      queryClient.setQueryData(["events", params.id], data.event); // optimistic updating
+  // const { mutate } = useMutation({
+  //   mutationFn: updateEvent,
+  //   onMutate: async (data) => {
+  //     //this data is arguments for mutate()
+  //     await queryClient.cancelQueries({ queryKey: ["events", params.id] });
+  //     const previousEvent = queryClient.getQueryData(["events", params.id]);
+  //     queryClient.setQueryData(["events", params.id], data.event); // optimistic updating
 
-      return { previousEvent }; //this will be context of onError()
-    },
-    onError: (error, data, context) => {
-      queryClient.setQueryData(["events", params.id], context.previousEvent); // roll back if the mutation fails
-    },
-    onSettled: () => {
-      queryClient.invalidateQueries(["events", params.id]) //clean cash to refetch data to be sure we synchronize with backend
-    }
-  });
+  //     return { previousEvent }; //this will be context of onError()
+  //   },
+  //   onError: (error, data, context) => {
+  //     queryClient.setQueryData(["events", params.id], context.previousEvent); // roll back if the mutation fails
+  //   },
+  //   onSettled: () => {
+  //     queryClient.invalidateQueries(["events", params.id]) //clean cash to refetch data to be sure we synchronize with backend
+  //   }
+  // });
 
   function handleSubmit(formData) {
-    mutate({ id: params.id, event: formData });
-    navigate("../");
+    // mutate({ id: params.id, event: formData });
+    // navigate("../");
+    submit(formData, { method: "PUT" }); //we a re not sending http request, just triggering action, and method should be not 'get'
   }
 
   function handleClose() {
@@ -43,13 +56,14 @@ export default function EditEvent() {
   }
   let content;
 
-  if (isPending) {
-    content = (
-      <div className="center">
-        <LoadingIndicator />
-      </div>
-    );
-  }
+  // with loader fn we should not meet pending state
+  // if (isPending) {
+  //   content = (
+  //     <div className="center">
+  //       <LoadingIndicator />
+  //     </div>
+  //   );
+  // }
   if (isError) {
     content = (
       <div className="center">
@@ -72,15 +86,37 @@ export default function EditEvent() {
   if (data) {
     content = (
       <EventForm inputData={data} onSubmit={handleSubmit}>
-        <Link to="../" className="button-text">
-          Cancel
-        </Link>
-        <button type="submit" className="button">
-          Update
-        </button>
+        {state === "submitting" ? (
+          <p>Sending data...</p>
+        ) : (
+          <>
+            <Link to="../" className="button-text">
+              Cancel
+            </Link>
+            <button type="submit" className="button">
+              Update
+            </button>
+          </>
+        )}
       </EventForm>
     );
   }
 
   return <Modal onClose={handleClose}>{content}</Modal>;
+}
+
+//to load component already having data
+export function loader({ params }) {
+  return queryClient.fetchQuery({
+    queryKey: ["events", params.id],
+    queryFn: ({ signal }) => fetchEvent({ signal, id: params.id }),
+  });
+}
+
+export async function action({ request, params }) {
+  const formData = await request.formData();
+  const updatedEventData = Object.fromEntries(formData); // gives us normal JS object
+  await updateEvent({ id: params.id, event: updatedEventData });
+  await queryClient.invalidateQueries(["events"]); //we have NO MORE optimistic update!!!
+  return redirect("../");
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,12 @@
+import { useIsFetching } from "@tanstack/react-query";
+
 export default function Header({ children }) {
+  const fetching = useIsFetching() //number = 0 if is not fetching and >0 if is fetching
   return (
     <>
-      <div id="main-header-loading"></div>
+      <div id="main-header-loading">
+       {fetching && <progress />} 
+      </div>
       <header id="main-header">
         <div id="header-title">
           <h1>React Events</h1>
