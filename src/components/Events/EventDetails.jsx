4cde2cf89340075106ff5eed4e0772b7475686a4
Oksana Kormilina <keysb@mail.ru>
2024-02-22 09:56:40 +0400

    Add invalidate, none refetch and some refactoring

--- src/components/Events/EventDetails.jsx
+++ src/components/Events/EventDetails.jsx
@@ -2,7 +2,7 @@ import { Link, Outlet, useParams, useNavigate } from "react-router-dom";
 import { useQuery, useMutation } from "@tanstack/react-query";
 
 import Header from "../Header.jsx";
-import { fetchEvent, deleteEvent } from "../../util/http.js";
+import { fetchEvent, deleteEvent, queryClient } from "../../util/http.js";
 import ErrorBlock from "../UI/ErrorBlock.jsx";
 
 export default function EventDetails() {
@@ -10,7 +10,7 @@ export default function EventDetails() {
   const navigate = useNavigate();
 
   const { data, isPending, isError, error } = useQuery({
-    queryKey: ["events", { search: id }],
+    queryKey: ["events", id],
     queryFn: ({ signal }) => fetchEvent({ id, signal }),
   });
 
@@ -22,6 +22,10 @@ export default function EventDetails() {
   } = useMutation({
     mutationFn: deleteEvent,
     onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["events"],
+        refetchType: "none"
+      });
       navigate("/events");
     },
   });
@@ -29,6 +33,58 @@ export default function EventDetails() {
   function handleDelete() {
     mutate({ id });
   }
+
+  let content;
+
+  if (isPending) {
+    content = (
+      <div id="event-details-content" className="center">
+        <p>Loading event details...</p>
+      </div>
+    );
+  }
+  if (isError) {
+    content = (
+      <div id="event-details-content" className="center">
+        <ErrorBlock
+          title="Could not fetch event details"
+          message={error.info?.message || "Could not fetch event details"}
+        />
+      </div>
+    );
+  }
+  if (data) {
+    const formattedDate = new Date(data.date).toLocaleDateString("en-US", {
+      day: "numeric",
+      month: "short",
+      year: "numeric",
+    });
+    content = (
+      <>
+        <header>
+          <h1>{data.title}</h1>
+          <nav>
+            <button onClick={handleDelete}>Delete</button>
+            <Link to="edit">Edit</Link>
+          </nav>
+        </header>
+
+        <div id="event-details-content">
+          <img src={`http://localhost:3000/${data.image}`} alt={data.image} />
+          <div id="event-details-info">
+            <div>
+              <p id="event-details-location">{data.location}</p>
+              <time dateTime={`Todo-DateT$Todo-Time`}>
+                {formattedDate} @ {data.time}
+              </time>
+            </div>
+            <p id="event-details-description">{data.description}</p>
+          </div>
+        </div>
+      </>
+    );
+  }
+
   return (
     <>
       <Outlet />
@@ -37,13 +93,7 @@ export default function EventDetails() {
           View all Events
         </Link>
       </Header>
-      {isPending && <p>Loading event details...</p>}
-      {isError && (
-        <ErrorBlock
-          title="Could not fetch event details"
-          message={error.info?.message || "Could not fetch event details"}
-        />
-      )}
+
       {isMutationPending && <p>Deleting event...</p>}
       {isMutationError && (
         <ErrorBlock
@@ -51,29 +101,8 @@ export default function EventDetails() {
           message={mutationError.info?.message || "Could not delete event"}
         />
       )}
-      {data && (
-        <article id="event-details">
-          <header>
-            <h1>{data.title}</h1>
-            <nav>
-              <button onClick={handleDelete}>Delete</button>
-              <Link to="edit">Edit</Link>
-            </nav>
-          </header>
-          <div id="event-details-content">
-            <img src={`http://localhost:3000/${data.image}`} alt={data.image} />
-            <div id="event-details-info">
-              <div>
-                <p id="event-details-location">{data.location}</p>
-                <time dateTime={`Todo-DateT$Todo-Time`}>
-                  {data.date} @ {data.time}
-                </time>
-              </div>
-              <p id="event-details-description">{data.description}</p>
-            </div>
-          </div>
-        </article>
-      )}
+
+      <article id="event-details">{content}</article>
     </>
   );
 }
